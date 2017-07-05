7c1728861c68bac0a0157a77f94deacfea2c388d
adrian <petricevicadrian@gmail.com>
2017-07-05 11:17:44 +0200

    Adding Stringdev class and blank test

---
+++ src/Model/Stringdev.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace src\Model;
+
+class Stringdev{
+
+    public function concat($str1, $str2){
+        return $strcat = $str1 . $str2;
+    }
+
+    public function toCamelCase($str1, $str2){
+        return $this->concat(ucfirst(strtolower($str1)),ucfirst(strtolower($str2)));
+    }
+
+    public function prefix($strpre, $strsuf, $camelcase = false){
+        if($camelcase == true){
+            return $this->concat(ucfirst(strtolower($strpre)),$strsuf);
+        }
+        return $this->concat($strpre,$strsuf);
+    }
+
+    public function suffix($strsuf, $strpre, $camelcase = false){
+        if($camelcase == true){
+            return $this->concat($strpre,ucfirst(strtolower($strsuf)));
+        }
+        return $this->concat($strpre,$strsuf);
+    }
+}
---
+++ tests/Model/StringdevTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests\Model;
+
+class StringdevTest extends \PHPUnit_Framework_TestCase {
+
+
+
+    public function testConcat() {
+
+    }
+
+    public function testToCamelCase(){
+
+    }
+
+    public function testPrefix(){
+
+    }
+
+    public function testSuffix(){
+
+    }
+}
