18cb937389ea4c8a65add27e455ffc2e3c680624
adrian <petricevicadrian@gmail.com>
2017-07-05 11:55:49 +0200

    Files fixed with php-cs-fixer

--- src/Model/Stringdev.php
+++ src/Model/Stringdev.php
@@ -2,30 +2,35 @@
 
 namespace Model;
 
-class Stringdev{
+class Stringdev
+{
+    public function __construct()
+    {
+    }
 
-
-    public function __construct(){}
-
-    public function concat($str1, $str2){
+    public function concat($str1, $str2)
+    {
         return $strcat = $str1 . $str2;
     }
 
-    public function toCamelCase($str1, $str2){
-        return $this->concat(ucfirst(strtolower($str1)),ucfirst(strtolower($str2)));
+    public function toCamelCase($str1, $str2)
+    {
+        return $this->concat(ucfirst(strtolower($str1)), ucfirst(strtolower($str2)));
     }
 
-    public function prefix($strpre, $strsuf, $camelcase = false){
-        if($camelcase == true){
-            return $this->concat(ucfirst(strtolower($strpre)),$strsuf);
+    public function prefix($strpre, $strsuf, $camelcase = false)
+    {
+        if ($camelcase == true) {
+            return $this->concat(ucfirst(strtolower($strpre)), $strsuf);
         }
-        return $this->concat($strpre,$strsuf);
+        return $this->concat($strpre, $strsuf);
     }
 
-    public function suffix($strsuf, $strpre, $camelcase = false){
-        if($camelcase == true){
-            return $this->concat($strpre,ucfirst(strtolower($strsuf)));
+    public function suffix($strsuf, $strpre, $camelcase = false)
+    {
+        if ($camelcase == true) {
+            return $this->concat($strpre, ucfirst(strtolower($strsuf)));
         }
-        return $this->concat($strpre,$strsuf);
+        return $this->concat($strpre, $strsuf);
     }
 }
--- tests/Model/StringdevTest.php
+++ tests/Model/StringdevTest.php
@@ -5,49 +5,56 @@ namespace tests\Model;
 use Model\Stringdev;
 use PHPUnit\Framework\TestCase;
 
-class StringdevTest extends TestCase {
-
+class StringdevTest extends TestCase
+{
     private $str1;
     private $str2;
 
-    public function setUp(){
+    public function setUp()
+    {
         $this->str1="provencal";
         $this->str2="legaullois";
     }
 
-    public function testConcat() {
+    public function testConcat()
+    {
         $str = new Stringdev();
-        $actual = $str->concat($this->str1,$this->str2);
-        $this->assertSame('provencallegaullois',$actual);
+        $actual = $str->concat($this->str1, $this->str2);
+        $this->assertSame('provencallegaullois', $actual);
     }
 
-    public function testToCamelCase(){
+    public function testToCamelCase()
+    {
         $str = new Stringdev();
-        $actual = $str->toCamelCase($this->str1,$this->str2);
-        $this->assertSame('ProvencalLegaullois',$actual);
+        $actual = $str->toCamelCase($this->str1, $this->str2);
+        $this->assertSame('ProvencalLegaullois', $actual);
     }
 
-    public function testPrefixFalse(){
+    public function testPrefixFalse()
+    {
         $str = new Stringdev();
-        $actual = $str->prefix($this->str1,$this->str2);
-        $this->assertSame('provencallegaullois',$actual);
+        $actual = $str->prefix($this->str1, $this->str2);
+        $this->assertSame('provencallegaullois', $actual);
     }
 
-    public function testPrefixTrue(){
+    public function testPrefixTrue()
+    {
         $str = new Stringdev();
-        $actual = $str->prefix($this->str1,$this->str2,true);
-        $this->assertSame('Provencallegaullois',$actual);
+        $actual = $str->prefix($this->str1, $this->str2, true);
+        $this->assertSame('Provencallegaullois', $actual);
     }
 
-    public function testSuffixFalse(){
+    public function testSuffixFalse()
+    {
         $str = new Stringdev();
-        $actual = $str->suffix($this->str2,$this->str1);
-        $this->assertSame('provencallegaullois',$actual);
+        $actual = $str->suffix($this->str2, $this->str1);
+        $this->assertSame('provencallegaullois', $actual);
     }
 
-    public function testSuffixTrue(){
+    public function testSuffixTrue()
+    {
         $str = new Stringdev();
-        $actual = $str->suffix($this->str2,$this->str1, true);
-        $this->assertSame('provencalLegaullois',$actual);
+        $actual = $str->suffix($this->str2, $this->str1, true);
+        $this->assertSame('provencalLegaullois', $actual);
     }
 }
